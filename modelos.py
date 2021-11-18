54a6e9f2b09399bac84034583a984cf576106459
Camilo Sabogal <lobogral@gmail.com>
2021-11-17 22:31:30 -0500

    Agrego modelo de precision

--- modelos.py
+++
@@ -1,5 +0,0 @@
-# Matriz de Confusión
-from sklearn.metrics import confusion_matrix
-y_true = ["gato", "lobo", "gato", "gato", "lobo", "perro"]  # i => Renglones
-y_pred = ["lobo", "lobo", "gato", "gato", "lobo", "gato"]  # j => Columnas
-print(confusion_matrix(y_true, y_pred, labels=["lobo", "perro", "gato"]))
